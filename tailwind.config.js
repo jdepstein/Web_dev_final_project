13b47b2575ab5cecad7cda51cc3a8cde75966699
jdepstein <jdepstein2020@gmail.com>
2023-02-08 13:37:15 -0500

    intial commit npm intial code

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/and-1-stats/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
---
+++ vendors/tailwindcss/stubs/simpleConfig.stub.js
@@ -0,0 +1,8 @@
+/** @type {import('vendors/tailwindcss/types').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
