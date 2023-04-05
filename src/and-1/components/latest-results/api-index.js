661b4d0cd46e089933772767f7bf4c875f468fa9
Calvin Crosby <calvincrosby@Calvins-MacBook-Air.local>
2023-04-05 14:54:23 -0400

    integrated api with latest results banner

---
+++ src/and-1/components/latest-results/api-index.js
@@ -0,0 +1,29 @@
+import results  from "./results.json"
+import ResultsItem from "./results-item";
+import games from "./recent_games.json";
+import ApiResultsItem from "./api-results-item";
+const ApiLatestResults = (
+) => {
+    let game_array = games.response;
+
+    console.log(game_array)
+
+    return (
+        <ul className="list-group list-group-horizontal border-top border-bottom">
+            {
+                game_array.map((game, i) => {
+                    if (i > 5) {
+                        return
+                    }
+                    else
+                        return (
+                            <li key={i} className="list-group-item text-dark m-auto border-0">
+                                <ApiResultsItem game={game}/>
+                            </li>)
+                })
+            }
+        </ul>
+    );
+
+};
+export default ApiLatestResults
--- src/and-1/schedule/index.js
+++ src/and-1/schedule/index.js
@@ -2,11 +2,12 @@ import Nav from "../nav";
 import NavigationSidebar from "../components/nav";
 import Standings from "../components/standings";
 import LatestResults from "../components/latest-results";
+import ApiLatestResults from "../components/latest-results/api-index";
 
 function Schedule() {
     return (
         <>
-            <LatestResults/>
+            <ApiLatestResults/>
             <div className="row p-0 m-0">
                 <NavigationSidebar/>
                 <div className="container-fluid col-9 col-lg-7 col-xl-8 p-0 border-start border-end align-content-center">
