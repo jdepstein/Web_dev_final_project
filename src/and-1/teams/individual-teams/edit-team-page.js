ce53bd74a3c43f7d950b0bfd2e2db58a161659f7
jdepstein <jdepstein2020@gmail.com>
2023-04-06 15:33:38 -0400

    Small edit

--- src/and-1/teams/individual-teams/edit-team-page.js
+++ src/and-1/teams/individual-teams/edit-team-page.js
@@ -1,10 +1,9 @@
 import {Link} from "react-router-dom";
 import {useParams} from "react-router";
-import {get_team} from "../../helper-funcs";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
 import {findIndividualTeamThunk} from "../../thunks/teams-thunks";
-import {findTeamUserThunk} from "../../thunks/team-users-thunks";
+import {findTeamUserThunk, updateTeamUserThunk} from "../../thunks/team-users-thunks";
 
 function EditTeamPage() {
     const { teamName } = useParams();
@@ -50,9 +49,13 @@ function EditTeamPage() {
                         </div>
                         <div className={teams.colors + " d-inline-block w-100"}>
                             <img alt="" className="opacity-50 ms-2 pt-1 pb-1 float-start a1-team-page-image" src={"../" + teams.logo}/>
-                            <Link className="text-decoration-none" to={"/teams/" + teamName}><button className="opacity-100 nav-item float-end rounded-pill a1-bg-blue shadow-none  fw-bold text-white btn-lg me-3 mt-4 ps-3 pe-3 pt-2 pb-2">
-                                Save
-                            </button></Link>
+                            <Link className="text-decoration-none" to={"/teams/" + teamName}>
+                                <button className="opacity-100 nav-item float-end rounded-pill a1-bg-blue shadow-none  fw-bold text-white btn-lg me-3 mt-4 ps-3 pe-3 pt-2 pb-2"
+                                        onClick={() => dispatch(updateTeamUserThunk({
+                                            ...team}))}>
+                                    Save
+                                </button>
+                            </Link>
                             <Link className="text-decoration-none" to={"/teams/" + teamName}><button className="opacity-100  nav-item float-end rounded-pill a1-bg-red shadow-none fw-bold text-white btn-lg me-3 mt-4 ps-3 pe-3 pt-2 pb-2">
                                 Cancel
                             </button></Link>
